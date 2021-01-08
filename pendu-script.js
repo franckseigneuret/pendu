e42986e1b5893f3baa07cbaa47dbe02f68fc46e2
Franck <franck@mail.com>
2021-01-08 15:09:36 +0100

    style + affichage message final

--- pendu-script.js
+++ pendu-script.js
@@ -4,6 +4,7 @@ const findBlock = document.getElementById('find-block')
 const checkletter = document.getElementById('check-letter')
 const show = document.getElementById('show')
 const suggestionLetter = document.getElementById('suggestion-letter')
+const alert = document.querySelector('.alert')
 
 const compteur = document.getElementById('count')
 let countValue = parseInt(compteur.textContent)
@@ -18,7 +19,7 @@ propositionSend.addEventListener('click', () => {
   word = document.getElementById('find-this-word').value
   wordArray = word.split('')
 
-  for(let i = 0; i <wordArray.length; i++){
+  for (let i = 0; i < wordArray.length; i++) {
     partialWord += '_'
   }
 
@@ -36,24 +37,53 @@ checkletter.addEventListener('click', () => {
     diminue()
   }
   suggestionLetter.value = ''
+
+  if (parseWordObj.success) {
+    showMessage('success')
+  }
 })
 
 parseWord = (letter) => {
-  console.log('letter recherchée = ', letter)
+  // console.log('letter recherchée = ', letter)
   let countLetterFind = 0
 
   wordArray.forEach((element, k) => {
     if (element === letter) {
-      console.log(element)
       countLetterFind++
-      
-      partialWord = partialWord.substring(0, k) + letter + partialWord.substring(k+1)
-    } 
+      partialWord = partialWord.substring(0, k) + letter + partialWord.substring(k + 1)
+    }
   })
-  return { partialWord, countLetterFind }
+  console.log("partialWord.indexOf('_') = ", partialWord.indexOf('_'))
+  let success = /\_/.test(partialWord) ? false : true // s'il n'y a plus d'underscore => success
+  return { partialWord, countLetterFind, success }
 }
 
 const diminue = () => {
   countValue--
   compteur.textContent = countValue
+
+  if (countValue === 0) {
+    showMessage('fail')
+  }
+}
+
+suggestionLetter.addEventListener('keyup', () => {
+  alert.textContent = suggestionLetter.value.length > 1 ? '1 seule lettre' : ''
+})
+
+// message victoire / perdu
+const showMessage = (type) => {
+  findBlock.querySelectorAll('input').forEach((el) => {
+    el.className = 'hidden'
+  })
+  const p = document.createElement('p')
+  if(type === 'success') {
+    p.className = 'victory'
+    p.textContent = 'Victory'
+  }
+  if(type === 'fail') {
+    p.className = 'fail'
+    p.textContent = `Perdu, le mot était : "${word}"`
+  }
+  show.append(p)
 }
